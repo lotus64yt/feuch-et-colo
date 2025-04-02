738e158cdf1f4a7363686d94d884813ff2fca1ab
lotus64 <114228798+lotus64yt@users.noreply.github.com>
2025-04-02 11:37:24 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['*']
+    domains: ['*',"feucherolles.fr",                   "scontent-cdg4-3.xx.fbcdn.net",]
   },
   redirects: async () => {
     return [
